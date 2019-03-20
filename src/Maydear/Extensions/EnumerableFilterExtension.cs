5a262a7afdd2afaef6cefaf752d71d95eab57ad3
kelvin <admin@maydear.com>
2019-03-21 00:10:27 +0800

    增加/修改开源协议头信息，增加随机函数助手

--- src/Maydear/Extensions/EnumerableFilterExtension.cs
+++ src/Maydear/Extensions/EnumerableFilterExtension.cs
@@ -1,4 +1,19 @@
-﻿using System;
+/*****************************************************************************************
+* Copyright 2014-2019 The Maydear Authors
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*****************************************************************************************/
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -21,6 +36,21 @@ namespace System.Collections.Generic
             if (collection == null)
             {
                 return true;
+            }
+
+            return !collection.Any();
+        }
+
+        /// <summary>
+        /// 是否不为null且是空集合
+        /// </summary>
+        /// <param name="collection">集合</param>
+        /// <returns></returns>
+        public static bool IsNotNullAndEmpty<T>(this IEnumerable<T> collection)
+        {
+            if (collection == null)
+            {
+                return false;
             }
 
             return !collection.Any();
@@ -54,16 +84,6 @@ namespace System.Collections.Generic
             }
 
             return collection.Any();
-        }
-
-        /// <summary>
-        /// 不为null或不是空集合
-        /// </summary>
-        /// <param name="collection">集合</param>
-        /// <returns>如果不是null对象或不是空集合则返回true，反之则为false</returns>
-        public static bool IsNotNullOrNotEmpty<T>(this IEnumerable<T> collection)
-        {
-            return !collection.IsNullOrEmpty();
         }
     }
 }
--- src/Maydear/Extensions/StringSecurityExtension..cs
+++ src/Maydear/Extensions/StringSecurityExtension..cs
@@ -1,5 +1,5 @@
 /*****************************************************************************************
-* Copyright 2014-2017 The Maydear Authors
+* Copyright 2014-2019 The Maydear Authors
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
