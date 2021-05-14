878acc950765a3484644265059b2e4ff8bb59302
Kelvin.Liang <admin@maydear.com>
2021-05-14 10:35:13 +0800

    增加支持191

---
+++ test/MaydearUnitTestCore/StringFilterExtensionUnitTest.cs
@@ -0,0 +1,16 @@
+using Maydear.Utilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace MaydearUnitTestCore
+{
+    [TestClass]
+    public class StringFilterExtensionUnitTest
+    {
+        [TestMethod]
+        public void NextLettersString()
+        {
+
+            System.Console.WriteLine("19120635417".IsChinaMobilePhone());
+        }
+
+    }
+}
